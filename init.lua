af676de2d72170dd4051031c06dc805174c7a98e
BotPhil01 <acsrobles39@gmail.com>
2025-03-10 15:18:13 +0000

    Feat: lspzero and mason support

--- init.lua
+++ init.lua
@@ -15,20 +15,28 @@ vim.opt.isfname:append("@-@")
 vim.g.mapleader= " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.cmd("map <leader>( vi(");
+vim.cmd("map <leader>) vi)");
+vim.cmd("map <leader>{ vi{");
+vim.cmd("map <leader>} vi}");
+vim.cmd("map <leader>[ vi[");
+vim.cmd("map <leader>] vi]");
+vim.cmd("map <leader>' vi'");
+vim.cmd('map <leader>" vi"');
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -39,14 +47,19 @@ local plugins = {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
         dependencies = {
             'nvim-lua/plenary.nvim'}
-        },
-        {'neovim/nvim-lspconfig'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/nvim-cmp'},
-        {'ThePrimeagen/harpoon',
+    },
+    {'ThePrimeagen/harpoon',
         dependencies = { 'nvim-lua/plenary.nvim' }},
-        {'mbbill/undotree'},
-    }
+    {'mbbill/undotree'},
+    {'nvim-treesitter/nvim-treesitter'},
+    {'VonHeikemen/lsp-zero.nvim'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {"williamboman/mason.nvim"},
+    {"williamboman/mason-lspconfig.nvim"},
+    {"neovim/nvim-lspconfig"},
+}
 
 local opts = {}
 require("lazy").setup(plugins, opts)
@@ -57,10 +70,10 @@ vim.keymap.set("n", "<leader>g", builtin.live_grep, { desc = 'Telescope grep' })
 
 local config = require("nvim-treesitter.configs")
 config.setup {
-  ensure_installed = { "lua","vim", "vimdoc", "c", "cpp", "javascript", "typescript", "java", "python", "markdown", "markdown_inline" },
-  auto_install = false,
- highlight = { enable = true },
- indent = { enable = true },
+    ensure_installed = { "lua","vim", "vimdoc", "c", "cpp", "javascript", "typescript", "java", "python", "markdown", "markdown_inline" },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
 }
 
 -- harpoon setup
@@ -72,4 +85,64 @@ require("nightfox").setup()
 vim.cmd("colorscheme nightfox")
 
 -- setup for undotree
-vim.keymap.set('n', '<leader><F5>', vim.cmd.UndotreeToggle)
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+-- setup for mason
+local mason = require('mason').setup()
+
+require('mason-lspconfig').setup({
+    ensure_installed = { 'lua_ls' },
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
+    },
+})
+
+-- setup for zerolsp
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+    'force',
+    lspconfig_defaults.capabilities,
+    require('cmp_nvim_lsp').default_capabilities()
+)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    end,
+})
+
+-- setup for autocomplete
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+    {
+        name = 'buffer',
+        option = 
+                {
+                    keyword_length = 1,
+                }
+    },
+  },
+  snippet = {
+    expand = function(args)
+      -- You need Neovim v0.10 to use vim.snippet
+      vim.snippet.expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({}),
+})
